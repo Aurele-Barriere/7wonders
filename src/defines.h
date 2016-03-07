a20f1ba1ddca29411ee83ff7a80a41ae699a4da8
Aurèle <aurele.barriere@gmail.com>
2016-03-07 21:53:23 +0100

    typos and comments

--- src/defines.h
+++ src/defines.h
@@ -4,7 +4,9 @@
 
 #define BOARD_SIZE 40
 #define NB_COLORS 7
-#define color1 -3
+// What symbol will be used for players. must be negative so there is no confusion with letters. 
+// To see wich symbol, add 97 and check ascii table.
+#define color1 -3 
 #define color2 -33
 
 #endif
